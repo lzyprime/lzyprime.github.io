ac42461e51001b0537f9224e5575dd5ce6ff9ba8
lzyprime <2383518170@qq.com>
2020-11-27 17:49:30 +0800

    更新master时，修改README.md

--- update_master.sh
+++ update_master.sh
@@ -3,6 +3,15 @@
 cp -r . ../tmp_master
 cd ../tmp_master
 rm -rf .git update_master.sh
+
+cat > README.md << EEE
+# I'm prime
+
+## 十方三世，尽在一念
+
+> update at: `date '+%Y-%m-%d %H:%M'`
+EEE
+
 git init
 git add .
 git commit -m "update`date '+%Y-%m-%d %H:%M'`"
